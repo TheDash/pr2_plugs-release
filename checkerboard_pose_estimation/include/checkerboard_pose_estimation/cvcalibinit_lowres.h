f7263a919033531c45ec3b67c693f2e91d025684
TheDash <noobaca2@gmail.com>
2014-10-14 12:06:48 -0400

    Trimmed the branch to only the pr2_image_snapshot_recorder sub directory

--- checkerboard_pose_estimation/include/checkerboard_pose_estimation/cvcalibinit_lowres.h
+++
@@ -1,8 +0,0 @@
-#if !defined(_CALIB_LOWRES_H)
-#define _CALIB_LOWRES_H
-
-#include <opencv/cv.h>
-
-int cvFindChessboardCornersLowres(IplImage* img, CvSize size, CvPoint2D32f* corners, int* corner_count = 0);
-
-#endif
